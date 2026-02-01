2835f1d7bad0eaa37c9b445ce043e8077a5a743e
doanngocnam <doanngocnam@DESKTOP-KJUJ077.localdomain>
2026-02-01 17:30:53 +0700

    fix status code

--- src/controllers/database.controller.ts
+++ src/controllers/database.controller.ts
@@ -1,14 +1,11 @@
 import { Request, Response } from 'express'
 import { DatabaseService } from '~/services/database.service.js'
-import { SuccessResponse } from '~/utils/success.response.js'
+import { OKResponse, SuccessResponse } from '~/utils/success.response.js'
 
 class DatabaseController {
 	clearAllData = async (req: Request, res: Response) => {
 		const result = await DatabaseService.clearAllData()
-		new SuccessResponse({
-			message: result.message,
-			metadata: result
-		}).send(res)
+		new OKResponse('Clear all data successfully!', 200, result).send(res)
 	}
 }
 
